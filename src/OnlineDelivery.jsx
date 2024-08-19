45dd6545865f47b9baffe0dd8038123bbafba492
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2024-08-19 17:01:19 +0530

    Update OnlineDelivery.jsx

--- src/OnlineDelivery.jsx
+++ src/OnlineDelivery.jsx
@@ -65,7 +65,7 @@ const OnlineDelivery = () => {
                     ))}
                 </div>
                 {isFixed && (<NavLink to='search'>
-                    <div className="relative">
+                    <div className="relative header-visibility">
                         <input
                             type="text"
                             placeholder="Search..."
